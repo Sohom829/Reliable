3a2598f82dcbc163c86f30f480393f5abe6026a4
Alpha5959 <109584578+Alpha5959@users.noreply.github.com>
2022-11-12 12:23:37 +0600

    Updated Giveaway
    Added various commands

--- commands/Giveaway/giveaway.js
+++ commands/Giveaway/giveaway.js
@@ -11,6 +11,7 @@ const {
 } = require("discord.js");
 const { execute } = require("./giveaway");
 const ms = require("ms");
+const config = require("../../config.json");
 const messages = require("../../utils/messages");
 
 module.exports = {
@@ -21,7 +22,7 @@ module.exports = {
     .addSubcommand((sub) =>
       sub
         .setName("start")
-        .setDescription("Start a giveaway")
+        .setDescription("🎉 Starts a giveaway")
         .addStringOption((option) =>
           option
             .setName("duration")
@@ -52,7 +53,7 @@ module.exports = {
     .addSubcommand((sub) =>
       sub
         .setName("reroll")
-        .setDescription("Reroll a giveaway")
+        .setDescription("🔃 Selects a new giveaway winner")
         .addStringOption((option) =>
           option
             .setName("giveaway")
@@ -62,8 +63,47 @@ module.exports = {
     )
     .addSubcommand((sub) =>
       sub
+        .setName("delete")
+        .setDescription("🚮 Deletes the giveaway")
+        .addStringOption((option) =>
+          option
+            .setName("giveaway-message-id")
+            .setDescription(
+              "The giveaway to pause (message ID or giveaway prize)"
+            )
+            .setRequired(true)
+        )
+    )
+    .addSubcommand((cmd) =>
+      cmd
+        .setName("pause")
+        .setDescription("⏸️ Pauses the giveaway")
+        .addStringOption((option) =>
+          option
+            .setName("giveaway-message-id")
+            .setDescription(
+              "The giveaway to pause (message ID or giveaway prize)"
+            )
+            .setRequired(true)
+        )
+    )
+    .addSubcommand((cmd) =>
+      cmd
+        .setName("unpause")
+        .setDescription("⏯️ Unpauses the giveaway")
+        .addStringOption((option) =>
+          option
+            .setName("giveaway-message-id")
+            .setDescription(
+              "The giveaway to unpause (message ID or giveaway prize)"
+            )
+            .setRequired(true)
+        )
+    )
+    .addSubcommand((sub) =>
+      sub
         .setName("end")
-        .setDescription("end a giveaway")
+        .setDescription("⏹️ Ends the giveaway")
         .addStringOption((option) =>
           option
             .setName("giveaway-message-id")
@@ -95,11 +135,40 @@ module.exports = {
         duration: ms(giveawayDuration),
         prize: giveawayPrize,
         winnerCount: giveawayWinnerCount,
+        hostedBy: interaction.user,
+        messages: {
+          giveaway:
+            (config.everyoneMention ? "@everyone\n\n" : "") +
+            "<a:reliable_rewards:1039201211196383333> **GIVEAWAY** <a:reliable_rewards:1039201211196383333>",
+          giveawayEnded:
+            (config.everyoneMention ? "@everyone\n\n" : "") +
+            "<a:reliable_rewards:1039201211196383333> **GIVEAWAY ENDED** <a:reliable_rewards:1039201211196383333>",
+          inviteToParticipate:
+            "> **React with <:reliable_giveaway:1038804433116614718> to participate!**",
+          dropMessage:
+            "> Be the first to react with React with <:reliable_giveaway:1038804433116614718> to participate!",
+          drawing: "> **`Ends in`**: {timestamp}",
+          winMessage: "**Congratulations, {winners}! You won `{this.prize}`**!",
+          embedFooter: "©2022 | Reliable",
+          noWinner:
+            "> **`Giveaway cancelled, Not enough entrants to determine a winner!`**",
+          hostedBy: `> **\`Hosted by\`**: ${interaction.user}`,
+          paused:
+            '"<a:reliable_alert:1039201857819648000> **GIVEAWAY PAUSED** <a:reliable_alert:1039201857819648000> ',
+          infiniteDurationText: "**`NEVER`**",
+          error: "> **`Reroll cancelled, no valid participations`**",
+          congrat:
+            "**New winner(s): {winners}! Congratulations, your prize is `{this.prize}`**!",
+          winners: "> **Winner(s)**",
+          endedAt: "Ended at",
+        },
       });
 
       const giveaway = new EmbedBuilder()
         .setTitle("Success")
-        .setDescription(`**Giveaway started in ${giveawayChannel}!**`)
+        .setDescription(
+          `✅ | **The giveaway for the \`${giveawayPrize}\` is starting in ${giveawayChannel}!**`
+        )
         .setColor("#0398fc")
         .setFooter({ text: "©2022 | Reliable" })
         .setTimestamp();
@@ -120,7 +189,9 @@ module.exports = {
       if (!giveaway) {
         const err_embed = new EmbedBuilder()
           .setTitle("Error")
-          .setDescription(`**Unable to find a giveaway for \`${query}\`.**`)
+          .setDescription(
+            `❌ | **Unable to find a giveaway for \`${query}\`.**`
+          )
           .setColor("#0398fc")
           .setFooter({ text: "©2022 | Reliable" })
           .setTimestamp();
@@ -129,8 +200,9 @@ module.exports = {
       }
 
       if (!giveaway.ended) {
-        const err_embed2 = new EmbedBuilder.setTitle("Error")
-          .setDescription(`**The giveaway is not ended yet.**`)
+        const err_embed2 = new EmbedBuilder()
+          .setTitle("Error")
+          .setDescription(`❌ | **The giveaway is not ended yet.**`)
           .setColor("#0398fc")
           .setFooter({ text: "©2022 | Reliable" })
           .setTimestamp();
@@ -143,7 +215,7 @@ module.exports = {
         .then(() => {
           const rerolled = new EmbedBuilder()
             .setTitle("Success")
-            .setDescription(`**Giveaway rerolled!**`)
+            .setDescription(`✅ | **Giveaway rerolled!**`)
             .setColor("#0398fc")
             .setFooter({ text: "©2022 | Reliable" })
             .setTimestamp();
@@ -156,6 +228,137 @@ module.exports = {
             ephemeral: true,
           });
         });
+    }
+
+    if (interaction.options.getSubcommand() === "pause") {
+      const getGiveaway = interaction.options.getString("giveaway-message-id");
+
+      const giveaway =
+        client.giveawaysManager.giveaways.find(
+          (g) => g.prize === getGiveaway && g.guildId === interaction.guild.id
+        ) ||
+        client.giveawaysManager.giveaways.find(
+          (g) =>
+            g.messageId === getGiveaway && g.guildId === interaction.guild.id
+        );
+
+      if (!giveaway) {
+        const err_embed = new EmbedBuilder()
+          .setTitle("Error")
+          .setDescription(
+            `❌ | Unable to find a giveaway for \`${getGiveaway}\``
+          )
+          .setColor("#0398fc")
+          .setFooter({ text: "©2022 | Reliable" })
+          .setTimestamp();
+
+        interaction.reply({ embeds: [err_embed], ephemeral: true });
+      }
+
+      if (giveaway.pauseOptions.isPaused) {
+        const err_embed2 = new EmbedBuilder()
+          .setTitle("Error")
+          .setDescription(`❌ | This giveaway is already paused.`)
+          .setColor("#0398fc")
+          .setFooter({ text: "©2022 | Reliable" })
+          .setTimestamp();
+
+        interaction.reply({ embeds: [err_embed2], ephemeral: true });
+      }
+
+      client.giveawaysManager.pause(giveaway.messageId).then(() => {
+        const paused = new EmbedBuilder()
+          .setTitle("Success")
+          .setDescription(`✅ | Giveaway paused!`)
+          .setColor("#0398fc")
+          .setFooter({ text: "©2022 | Reliable" })
+          .setTimestamp();
+
+        interaction.reply({ embeds: [paused], ephemeral: true });
+      });
+    }
+
+    if (interaction.options.getSubcommand() === "unpause") {
+      const getGiveaway = interaction.options.getString("giveaway-message-id");
+
+      const giveaway =
+        client.giveawaysManager.giveaways.find(
+          (g) => g.prize === getGiveaway && g.guildId === interaction.guild.id
+        ) ||
+        client.giveawaysManager.giveaways.find(
+          (g) =>
+            g.messageId === getGiveaway && g.guildId === interaction.guild.id
+        );
+
+      if (!giveaway) {
+        const err_embed = new EmbedBuilder()
+          .setTitle("Error")
+          .setDescription(
+            `❌ | Unable to find a giveaway for \`${getGiveaway}\``
+          )
+          .setColor("#0398fc")
+          .setFooter({ text: "©2022 | Reliable" })
+          .setTimestamp();
+
+        interaction.reply({ embeds: [err_embed], ephemeral: true });
+      }
+
+      if (!giveaway.pauseOptions.isPaused) {
+        const err_embed = new EmbedBuilder()
+          .setTitle("Error")
+          .setDescription(`❌ | This giveaway is not paused.`)
+          .setColor("#0398fc")
+          .setFooter({ text: "©2022 | Reliable" })
+          .setTimestamp();
+
+        interaction.reply({ embeds: [err_embed], ephemeral: true });
+      }
+
+      client.giveawaysManager.unpause(giveaway.messageId).then(() => {
+        const unpaused = new EmbedBuilder()
+          .setTitle("Success")
+          .setDescription(`✅ | Giveaway unpaused!`)
+          .setColor("#0398fc")
+          .setFooter({ text: "©2022 | Reliable" })
+          .setTimestamp();
+
+        interaction.reply({ embeds: [unpaused], ephemeral: true });
+      });
+    }
+    if (interaction.options.getSubcommand() === "delete") {
+      const getGiveaway = interaction.options.getString("giveaway-message-id");
+      const giveaway =
+        client.giveawaysManager.giveaways.find(
+          (g) => g.prize === getGiveaway && g.guildId === interaction.guild.id
+        ) ||
+        client.giveawaysManager.giveaways.find(
+          (g) =>
+            g.messageId === getGiveaway && g.guildId === interaction.guild.id
+        );
+
+      if (!giveaway) {
+        const err_embed = new EmbedBuilder()
+          .setTitle("Error")
+          .setDescription(
+            `❌ | Unable to find a giveaway for \`${getGiveaway}\``
+          )
+          .setColor("#0398fc")
+          .setFooter({ text: "©2022 | Reliable" })
+          .setTimestamp();
+
+        interaction.reply({ embeds: [err_embed], ephemeral: true });
+      }
+
+      client.giveawaysManager.delete(giveaway.messageId).then(() => {
+        const deleted = new EmbedBuilder()
+          .setTitle("Success")
+          .setDescription(`✅ | Deleted the Giveaway!`)
+          .setColor("#0398fc")
+          .setFooter({ text: "©2022 | Reliable" })
+          .setTimestamp();
+
+        interaction.reply({ embeds: [deleted], ephemeral: true });
+      });
     }
 
     if (interaction.options.getSubcommand() === "end") {
@@ -171,7 +374,9 @@ module.exports = {
       if (!giveaway) {
         const err_embed = new EmbedBuilder()
           .setTitle("Error")
-          .setDescription(`**Unable to find a giveaway for \`${query}\`.**`)
+          .setDescription(
+            `❌ | **Unable to find a giveaway for \`${query}\`.**`
+          )
           .setColor("#0398fc")
           .setFooter({ text: "©2022 | Reliable" })
           .setTimestamp();
@@ -181,7 +386,7 @@ module.exports = {
 
       if (giveaway.ended) {
         const err_embed2 = new EmbedBuilder.setTitle("Error")
-          .setDescription(`**This giveaway is already ended.**`)
+          .setDescription(`❌ | **This giveaway is already ended.**`)
           .setColor("#0398fc")
           .setFooter({ text: "©2022 | Reliable" })
           .setTimestamp();
@@ -192,7 +397,7 @@ module.exports = {
       client.giveawaysManager.end(giveaway.messageId).then(() => {
         const ended = new EmbedBuilder()
           .setTitle("Success")
-          .setDescription(`**Giveaway ended!**`)
+          .setDescription(`**✅ | Giveaway ended!**`)
           .setColor("#0398fc")
           .setFooter({ text: "©2022 | Reliable" })
           .setTimestamp();
@@ -207,8 +412,9 @@ module.exports = {
     }
   },
 };
+
 /**
  * @Author Reliable Inc.
  * @Copyright ©2022 | Reliable Inc, All rights reserved.
  * @CodedBy Mohtasim Alam Sohom, Sajidur Rahman Tahsin
- */
+ **/
